51ca3288a852abb7c7f45a208b1f6c62f0f84945
Rodrigo Rocha <rocha.webdev@gmail.com>
2024-11-10 15:12:42 -0300

    chore: update peer dependencies

--- dist/template/html.d.ts
+++ dist/template/html.d.ts
@@ -1,4 +1,4 @@
 import type { HTMType } from './types';
-declare const html: (strings: TemplateStringsArray, ...values: any[]) => HTMType | HTMType[];
+declare const html: (strings: TemplateStringsArray, ...values: any[]) => HTMType<any, any, any> | HTMType<any, any, any>[];
 export { html };
 //# sourceMappingURL=html.d.ts.map
