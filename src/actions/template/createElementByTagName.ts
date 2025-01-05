ae6659641c3972099a12fb96802c1198387625e0
Rodrigo Rocha <rocha.webdev@gmail.com>
2025-01-05 20:27:46 -0300

    fix: fix css selector class name

--- src/actions/template/createElementByTagName.ts
+++ src/actions/template/createElementByTagName.ts
@@ -3,24 +3,30 @@ import type { TemplateSchema } from "@/template";
 import { setElementAttributes } from "./setElementAttributes";
 import { renderChildren } from "./renderChildren";
 
+const _extractHashId = (text: string) => {
+  const regex = /(_.*)+/gi;
+  if (!text) return text;
+  return text.replace(regex, "");
+};
 export const createElementByTagName =
   (template: TemplateSchema, parentElement: Element, state: State = {}) =>
-  () => {
-    const tagName = template.type as string;
-    const selector = tagName.toLowerCase();
-    const element = document.createElement(tagName);
-    const hashId = parentElement.getAttribute("class") as string;
-    const className = template?.props?.class as string;
-    if (className) {
-      if (!className.includes(hashId)) {
-        const newClassName = `${hashId}_${className}`;
-        setElementAttributes(element, { class: newClassName });
-        parentElement.insertAdjacentElement("beforeend", element);
-        renderChildren(template.children, element, state);
-        return;
+    () => {
+      const tagName = template.type as string;
+      const selector = tagName.toLowerCase();
+      const element = document.createElement(tagName);
+      const parentElementClass = parentElement.getAttribute("class") as string;
+      const hashId = _extractHashId(parentElementClass);
+      const className = template?.props?.class as string;
+      if (className) {
+        if (!className.includes(hashId)) {
+          const newClassName = `${hashId}_${className}`;
+          setElementAttributes(element, { class: newClassName });
+          parentElement.insertAdjacentElement("beforeend", element);
+          renderChildren(template.children, element, state);
+          return;
+        }
       }
-    }
-    setElementAttributes(element, template.props);
-    parentElement.insertAdjacentElement("beforeend", element);
-    renderChildren(template.children, element, state);
-  };
+      setElementAttributes(element, template.props);
+      parentElement.insertAdjacentElement("beforeend", element);
+      renderChildren(template.children, element, state);
+    };
